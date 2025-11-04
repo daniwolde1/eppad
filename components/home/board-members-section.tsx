bfa0e993fdcc24ab9ffff08169cac0ee7f10f43b
daniwolde1 <66035780+daniwolde1@users.noreply.github.com>
2025-11-04 16:56:45 +0300

    Update board-members-section.tsx

--- components/home/board-members-section.tsx
+++ components/home/board-members-section.tsx
@@ -15,7 +15,7 @@ const boardMembers = [
   {
     name: "Dr. Raniya Al-Matari",
     position: "Vice-President",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/07/raniya-1.png",
+    image: "/raniya-1.jpg",
     bio: "Raniya Ali Al-Matari was born in the United States to a Yemini father and Ethiopian mother and raised in the Middle East capital of Saudi Arabia, Riyadh. She is fluent in five different languages: English, Arabic, Amharic, Tigrinya, and Urdu. She completed her pharmacy degree at Jordan University of Science and Technology in 2008 and pursued a Ph.D. in Pharmacy Administration at Howard University. Her dissertation focused on developing and validating an Amharic version Simplified Medication Adherence Questionnaire. She is a Board-Certified Pharmacist in Saudi Arabia and Board-Certified Pharmacy Technician in the US. Currently teaching at Howard University as a Biostatistics instructor, her research interests include pharmacoepidemiology, health outcome research, and secondary data analysis.",
     linkedin: "#",
     twitter: "#",
@@ -23,7 +23,7 @@ const boardMembers = [
   {
     name: "Prof. Bisrat Hailemeskel",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/08/pro.-bisrat.jpg",
+    image: "/pro.bisrat.jpg",
     bio: "Dr. Bisrat Hailemeskel is a Full Professor & Co-Director of International Grant in the College of Pharmacy, Howard University. He received his Bachelor of Pharmacy and Master of Science Degrees from Addis Ababa University, Ethiopia and Doctor of Pharmacy Degree from the University of Toledo, Ohio. He was the recipient of the 2007-2008 Fulbright Scholarship as a teacher/research fellow. As a Principal Investigator, he received over a million dollars grant for the 'Howard-Addis Ababa University Twinning Partnership' project through PEPFAR. He has over 50 research papers published in peer-reviewed journals and over 100 presentations at major scientific conferences. He was the founder and past president of the Ethiopian Pharmacists Association in North America (EPANA).",
     linkedin: "#",
     twitter: "#",
@@ -31,7 +31,7 @@ const boardMembers = [
   {
     name: "Mr. Gabriel Daniel",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/07/Gabriel-e1657114665624.jpg",
+    image: "/Gabriel-e1657114665624.jpg",
     bio: "Gabriel Daniel (aka: Aklile G. Giorgis) is a development/pharmaceutical management systems and services specialist with over 40 years of global experience residing in the USA. His technical areas of expertise include pharmaceutical management, primary health care, HIV/AIDS, neglected tropical diseases (NTDs), micro-enterprise development, and post-Ebola pharmaceutical systems recovery. He worked for over fifteen years as Principal Technical Advisor/Country Strategic Lead for pharmaceutical projects at Management Sciences for Health (MSH). He established the Sierra Leone and Ghana Africare country offices and the Ethiopia MSH operations which implemented USAID funded PEPFAR HIV/AIDS and PMI malaria programs.",
     linkedin: "#",
     twitter: "#",
@@ -39,7 +39,7 @@ const boardMembers = [
   {
     name: "Dr. Alemayehu Akalu",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/08/dr-alex-2.png",
+    image: "/dr.alex.png",
     bio: "Dr. Alex Akalu is a pharmacist-scientist with over 15 years of experience blending science, regulation, and technology. With a foundation in physics, he specializes in regulatory science, pediatric oncology, and applied artificial intelligence (AI) in healthcare. He currently serves as a Senior Oncology Staff Fellow at the U.S. Food and Drug Administration (FDA), leading data analysis, clinical research, and AI integration for the Division of Pediatric Oncology. Dr. Akalu was instrumental in developing the Pediatric Molecular Target List, a globally utilized resource guiding pediatric cancer drug development. His peer-reviewed research has been published in JAMA Oncology, Pediatric Blood & Cancer, and The Journal of Clinical Oncology.",
     linkedin: "#",
     twitter: "#",
@@ -47,7 +47,7 @@ const boardMembers = [
   {
     name: "Dr. Fekadu Fullas",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/08/dr.-fekadu.jpg",
+    image: "/dr.fekadu.jpg",
     bio: "Dr. Fekadu graduated from Addis Ababa University with a Bachelor of Pharmacy degree and studied Natural Products Chemistry at the University of Illinois at Chicago (UIC) College of Pharmacy where he obtained a Ph.D. degree. He joined the Research Triangle Institute (RTI) in North Carolina as a post-doctoral chemist, eventually becoming a clinical pharmacist at St. Luke's Hospital in Sioux City, Iowa for 23 years. He is an Associate Fellow of the Ethiopian Academy of Sciences. In the course of his long career, he has produced over 175 publications and is the author of eight books. He retired in 2020 but stays active in academic research, writing books and volunteering in pharmacy-related initiatives.",
     linkedin: "#",
     twitter: "#",
@@ -63,7 +63,7 @@ const boardMembers = [
   {
     name: "Dr. Pawlose Ketema",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/07/Dr_Pawlose_Ketema.png",
+    image: "/dr.pawlos.jpg",
     bio: "Dr. Pawlose Ketema is an Antimicrobial Stewardship pharmacist at the Washington DC VA Medical Center. He earned his Doctor of Pharmacy degree from Howard University College of Pharmacy, completed a PGY-1 Pharmacy residency at the Washington DC VA Medical Center followed by a PGY-2 residency in Infectious Diseases at Boston Medical Center. Dr. Ketema is a Board-Certified Pharmacotherapy Specialist (BCPS), Board Certified Infectious Diseases Pharmacist (BCIDP), and an American Academy of HIV Pharmacist (AAHIVP). He has authored and presented his research at regional and national conferences including ASHP, ACCP, and IDWeek. He serves on the EPPAD board as an advisory member and co-leads EPPAD's Educational Group.",
     linkedin: "#",
     twitter: "#",
@@ -71,7 +71,7 @@ const boardMembers = [
   {
     name: "Dr. Eyob Hailu",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/08/mr-eyob.jpg",
+    image: "/dr.eyob.jpg",
     bio: "Dr. Eyob is a versatile Clinical Pharmacist with broad experience across academia, retail, hospital and research settings. He earned Bachelor of Pharmacy and Master of Science degree in Experimental Pharmacology at Addis Ababa University, Ethiopia and Doctor of Pharmacy Degree from Howard University, Washington, DC. He worked in the chemo-informatics and drug design laboratory as a research fellow and has been involved in the publication of four articles in reputable pharmaceutical science journals. He worked as Clinical Pharmacist in HIV Specialty Clinic and Behavioral health centers in the Washington metropolitan area. He is an active member of International Brain Research Organization (IBRO), Society of Neuroscientist (SONA), and American Pharmacist Association (APhA).",
     linkedin: "#",
     twitter: "#",
@@ -79,7 +79,7 @@ const boardMembers = [
   {
     name: "Dr. Samuel Worku",
     position: "Treasury",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/07/Samuel-Worku.png",
+    image: "/Samuel-Worku.png",
     bio: "Samuel Worku was born and raised in Addis Ababa, Ethiopia. He graduated from Unity University College of Business Administration in 2008 and later attended George Mason University in Fairfax, VA, receiving his bachelor's degree double majored in Finance and Accounting. While at George Mason, he was a member of the Investment Club and The National Society of Collegiate Scholars. Currently, he works for Robert Half International as a financial consultant where he provides help to multiple companies in areas of budget analysis and forecasting, financial reporting, tax filing, audit, A/P and A/R process.",
     linkedin: "#",
     twitter: "#",
@@ -87,7 +87,7 @@ const boardMembers = [
   {
     name: "Dr. Helen HaileSelassie",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/07/Helen1.jpg",
+    image: "/Helen1.jpg",
     bio: "Dr. Helen HaileSelassie, PharmD, is a residency-trained and board-certified pharmacist with over 10 years of experience working in the healthcare and pharmaceutical industry. In her current role as a clinical manager, she oversees the development of formulary submission documents for managed care decision makers and participates in health economics and outcomes research. She has expertise in clinical pharmacy practice, pharmacy leadership, and developing clinical protocols using evidence-based medicine. She graduated from Virginia Commonwealth University with a Doctor of Pharmacy degree and completed her PGY-1 pharmacy practice residency training. She is a member of the Academy of Managed Care Pharmacy and the Industry Pharmacists Organization.",
     linkedin: "#",
     twitter: "#",
@@ -95,7 +95,7 @@ const boardMembers = [
   {
     name: "Dr. Eyerusalem Befkadu",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/07/eyerusalem.jpg",
+    image: "/eyerusalem.jpg",
     bio: "Dr. Eyerusalem Befkadu, PharmD, BCPS is an emergency medicine clinical specialist at Medstar Georgetown University Hospital. She practices as a pharmacist clinician providing safe and effective care of patients, primarily in the emergency department. She participates in quality improvement initiatives through the development of institutional clinical guidelines and protocols. She successfully implemented a naloxone dispensing program in the emergency department as part of an initiative to improve safe access to life-saving medications. She received her Doctor of Pharmacy degree from Touro College of Pharmacy and completed her PGY-1 pharmacy practice residency training at Howard University Hospital.",
     linkedin: "#",
     twitter: "#",
@@ -103,7 +103,7 @@ const boardMembers = [
   {
     name: "Dr. Sarem Hailemariam",
     position: "Board Member",
-    image: "http://eppad1.eppad.org/wp-content/uploads/2025/08/sarem3.jpg",
+    image: "/sarem3.jpg",
     bio: "Dr. Sarem Hailemariam is a dedicated board member of EPPAD, contributing expertise in pharmaceutical sciences and healthcare to advance the organization's mission of promoting public health and professional development among Ethiopian pharmacists and pharmaceutical scientists in the diaspora.",
     linkedin: "#",
     twitter: "#",
